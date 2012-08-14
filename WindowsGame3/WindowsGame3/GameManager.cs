7b3199da9cbddd46dd0a0b726deb05b38cb70fad
Tom Sela <tsela85@gmail.com>
2012-08-14 23:20:25 +0300

    commit

--- WindowsGame3/WindowsGame3/GameManager.cs
+++ WindowsGame3/WindowsGame3/GameManager.cs
@@ -26,7 +26,7 @@ class GameManager
     Board board;
     Table table;
     bool prefold;
-    ScreenState screen = ScreenState.game;
+    ScreenState screen = ScreenState.start;
     Texture2D startScreen;
     Texture2D helpScreen;
     Texture2D levelScreen;
